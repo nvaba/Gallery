80e3221308cd10005a5b103b049a84747e1ddde6
Nate <nateaba@gmail.com>
2023-06-15 14:13:39 -0700

    Edit placeholder content queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <main className="px-8 py-12">
-      <h1 className="text-[2.5rem]">Nate Abaria</h1>
-      <p className="text-xl">hello</p>
+    <main className="px-8 py-12 lg:px-36 lg:py-28">
+      <h1 className="text-[2.5rem] lg:text-5xl">Nate Abaria</h1>
+      <p className="text-xl">Coming Soon</p>
     </main>
   );
 }
