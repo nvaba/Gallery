a2d4181e9c4cfcb7299f4e0b0359e6033ba60dd1
Nate <nateaba@gmail.com>
2023-06-15 18:32:22 -0700

    Prep launch for live

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <main className="px-8 py-12 lg:px-36 lg:py-28">
       <h1 className="text-[2.5rem] lg:text-5xl">Nate Abaria</h1>
-      <p className="text-xl">Coming Soon</p>
+      <p className="text-xl">Under construction</p>
     </main>
   );
 }
