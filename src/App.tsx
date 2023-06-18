140983f82c9243a6ef22eb5738c0ba8015ce392a
Nate <nateaba@gmail.com>
2023-06-17 21:08:49 -0700

    Import header and photography component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header";
+import Photography from "./components/Photography";
 function App() {
   return (
-    <main className="px-8 py-12 lg:px-36 lg:py-28">
-      <h1 className="text-[2.5rem] lg:text-5xl">Nate Abaria</h1>
-      <p className="text-xl">Under construction</p>
+    <main className="px-8 py-16">
+      <Header />
+      <Photography />
     </main>
   );
 }
