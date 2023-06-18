a196f3583f553a24db11c5e6fb1dc462256dd9f1
Nate <nateaba@gmail.com>
2023-06-18 16:22:12 -0700

    Add footer content

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,30 @@
 function Footer() {
   return (
-    <footer>
-      <div>test</div>
+    <footer className="sm:flex sm:justify-between">
+      <nav>
+        <a target="_blank" href="mailto:nateaba@gmail.com">
+          nateaba@gmail.com
+        </a>
+        <div className="flex gap-1">
+          <a
+            target="_blank"
+            href="https://www.linkedin.com/in/nate-abaria-891b34243/"
+          >
+            <img className="h-7" src="/linkedin.svg" alt="LinkedIn" />
+          </a>
+          <a target="_blank" href="https://github.com/nvaba">
+            <img className="h-7" src="/github.svg" alt="Github" />
+          </a>
+        </div>
+      </nav>
+
+      <nav className="mt-5 sm:mt-0">
+        <a target="_blank" href="https://nateabaria.ca">
+          <span className="flex gap-1">
+            Portfolio <img src="/right.svg" alt="Right Arrow" />
+          </span>
+        </a>
+      </nav>
     </footer>
   );
 }
