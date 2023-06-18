f7518e010ff605a66eb00b55c9f05206ad762208
Nate <nateaba@gmail.com>
2023-06-18 16:24:04 -0700

    Add intro text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,7 @@ function Header() {
     <header>
       <h1 className="text-3xl xl:text-5xl">Nate Abaria</h1>
       <p className="xl:text-xl">Selected Work</p>
+      <p className="mt-2">A collection of freelance and fun</p>
     </header>
   );
 }
